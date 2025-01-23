66c83be41d8477430bca26c3bf9dbfd78cb3676a
Ankit Kumar <ankitkr5447@gmail.com>
2025-01-23 12:53:08 +0530

    Greater of three distnict integers

---
+++ conditionalStatements/GreatestOfThree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int a,b,c;
+    printf("Enter three numbers a b c");
+    scanf("%d %d %d",&a,&b,&c);
+    if (a>b && a>c){
+        printf("%d is greater.",a);
+    }else if (b>a && b>c){
+        printf("%d is greater.",b);
+    }else if(c>a && c>b){
+        printf("%d is greater.",c);
+    } else {
+        printf("Please enter distnict numbers.");
+    }
+}
