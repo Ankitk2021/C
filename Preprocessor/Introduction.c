cdeeab1b1aabe3c64e621f97ecfb9853db7e73bc
Ankit Kumar <ankitkr5447@gmail.com>
2025-01-23 15:23:01 +0530

    Preprocessor

---
+++ Preprocessor/Introduction.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <limits.h>
+void main (){
+    /*
+     #       --> preprocessor
+    #include --> preprocessor directive
+     stdio   --> standard input and output
+     math.h  --> square like function include
+     limit.h --> max or min find 
+    */
+
+   float x = sqrt(2);
+   float y = pow(2,3);
+   int z = INT_MAX;
+   printf("%f %f %d",x,y,z);
+}
