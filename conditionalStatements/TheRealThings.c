1beaaf85b2a53d2119341077f3a34500cdb3e37a
Ankit Kumar <ankitkr5447@gmail.com>
2025-01-23 15:23:01 +0530

    Strange things in c

---
+++ conditionalStatements/TheRealThings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int a;
+    if(3+2%5)
+        printf("This works.");
+        if(a=10)
+         printf("This works too.");
+         if(-5) printf("wow this works too.🤨");
+         if('a') printf("Worked.");
+}
