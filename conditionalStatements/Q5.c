947907976bb248742324d65f3ac35a36f83a2107
Ankit Kumar <ankitkr5447@gmail.com>
2025-01-23 12:32:35 +0530

    Check given number is divisble by 5 or 3 , by 3 ,by 5 , by both

---
+++ conditionalStatements/Q5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+
+int num1;
+printf("Enter the number : ");
+scanf("%d",&num1);
+if(num1%3==0 && num1%5==0){
+
+   printf("divisible by both 5 and 3"); 
+}else if (num1%3==0 ){
+    printf("divisible by 3");
+
+} else if (num1%5==0){
+    printf("divisible by 5");
+} else {
+    printf("divisible by neither 3 nor 5");
+}
+
+}
