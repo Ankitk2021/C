818918a587c2496379281b3a14b99debd0b7b9b4
Ankit Kumar <ankitkr5447@gmail.com>
2025-01-24 11:45:26 +0530

    Macro and Macro Function

---
+++ MacrosAndMacroFunction/Intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define Pi2  3.14338776734312
+void main(){
+    /*
+    #define is macro.It is define to permanently.It is similar to type def.
+#define Pi 3.143387493938
+#define Area(r) (Pi*r*r)
+
+    */
+ 
+
+   printf(" %.12f ",Pi2);
+}
